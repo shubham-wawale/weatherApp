416fda2759fa551d457cd54a91c59b263857595a
scodes11 <wawaleshubham@gmail.com>
2021-02-26 03:11:26 +0530

    changes in api.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const apiKey = require("./api");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,7 +12,7 @@ app.get("/", function(req,res){
 
 app.post("/", function(req,res){
     const query = req.body.cityName;  
-    var url ="https://api.openweathermap.org/data/2.5/weather?q="+ query + "&appid=" + apiKey ;
+    var url ="https://api.openweathermap.org/data/2.5/weather?q="+ query + "&appid=" + apiKey.Key ;
     https.get(url, function(response){
        console.log(response.statusCode);
        response.on("data", function(data){
